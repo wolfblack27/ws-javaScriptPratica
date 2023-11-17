225310c1cb64ccae40900b3841f5d555d4332628
wolfblack27 <tigowolfblack27@gmail.com>
2023-11-16 23:21:07 -0300

    Aplicacao da pratica01

--- Pratica_Function_01/index.js
+++ Pratica_Function_01/index.js
@@ -3,19 +3,28 @@
 /**
  * Função nominal
  */
-function media(){
-
+function media(notaMedia) {
+return notaMedia>6? true:false
 
 }
 
 /* Função do tipo Arrow*/
-condicaoTrabalho = (notaTrabalho)=>{
-
-
+condicaoTrabalho = (notaTrabalho) => {
+    return notaTrabalho > 0 ? true : false
 }
+
 
 /** Função anonima, onde condicaoAluno que aponta para ela */
-condicaoAluno = function(){
+condicaoAluno = function () {
+const trabalhoNota = 5
+const mediaNota = 7
+if(condicaoTrabalho(trabalhoNota)&& media(mediaNota)){
 
+    console.log("Passou")
+}else {
+    console.log("Não passou de ano")
+}
 
 }
+
+condicaoAluno()
